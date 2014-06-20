fec84a71bfe2baa2f6a4092727b6e7a65b9fbccc
Sue <sa.brayshaw@gmail.com>
2014-06-20 17:21:50 +0100

    GCD Assignment
    Codebook, narrow_tidy_data and run_analysis.r for Assignment

---
+++ Codebook.md
@@ -0,0 +1,95 @@
+########################################################################################
+####     Getting & Cleaning Data Project  Codebook 20.06.2014
+####
+####     Program written using version 3.0.3 of R, version 0.98.501 of R Studio
+####     and the Windows 8 operating system.
+####
+####	 INPUT
+####     Original data taken from the course assignment web-site at
+####	 https://class.coursera.org/getdata-004/human_grading/view/courses/972137/assessments/3/submissions
+####	 downloaded on 10.06.2014 and copied to the working directory. This in turn came from 
+####	 the accelerometers from the Samsung Galaxy S smartphone at
+####     http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones 
+####
+####	 OUTPUT
+####	 narrow_tidy_data.txt in the working directory, overwritten each time, this can be read into R using
+####		read.table("narrow_tidy_data.txt") providing it is in the working directory
+####	 
+########################################################################################
+
+A full description of the original data is available at the site where the data was obtained:
+http://archive.ics.uci.edu/ml/datasets/Human+Activity+Recognition+Using+Smartphones, I have not 
+included this information here incase of duplication errors.
+
+The files downloaded are:
+
+The training data - subject_train.txt (subject ids), X_train.txt (movement vectors) y_train.txt (activity ids)
+The test data - subject_test.txt (subject ids) X_test.txt (movement vectors) y_test.txt (activity ids)
+Features.txt containing the list of variable names used for X_train and X_test, these 
+	names were not changed during processing so that they can provide an audit trail back to the 
+	original data and are found in the 'Movement_type' column of the output data.
+Activities.txt provided the list of Activities and their level.
+
+libraries required
+	plyr
+	reshape2
+
+Variables used in the code:
+
+Generally names kept as close to the original as possible.
+
+The original data was read into the following data.frames
+X_test - data from X_test.txt
+y_test - data from y_test.txt
+test_subject - data  from subject_test.txt
+X_train - data from X_train.txt
+y_train - data from y_train.txt
+train_subject - data  from subject_train.txt
+activities - data from activity_labels.txt
+features - data from features.txt 
+
+Variables used during processing - each time the data was changed it was written to a different data set
+with a name reflecting the changes made
+
+VarNames - list of character strings containing the names for the X_test and X_train data  
+		taken from the second column of activities
+train - data.frame containing the combined train data
+test - data.frame containing the combined test data
+all_data - data.frame containing the combined test and train data
+m_data - data.frame containing the columns from all_data with 'mean' in the name
+mean_data - data frame containing m_data with 'meanFreq' columns removed
+	(This appeared to be the general consensus from the discussion forum)
+std_data - data frame containing just the columns from all_data with 'std' in the name
+tidy_data - data frame containing the combined subject, activity, mean_data and std_data
+mtd - data frame containing the first melt of tidy_data into 679734 observations 
+	of the 4 variables, subject, activity, variable and value
+wide_tidy_data - data frame cast from mtd, containing the average data for each movement
+	it has 180 observations of 68 variables, one row for each subject and activity, 
+	with 66 measurements for each.
+narrow_tidy_data - dataframe melted from wide_tidy_data, containing 
+	11880 observations of 4 variables, one row for each subject, activity and measurement type
+
+Output
+
+narrow_tidy_data.txt - text file containing the narrow_tidy_data dataframe, each output overwrites 
+	the previous one
+ 
+Summary choices made
+
+read.table() used to read all the data into the program from txt files
+names() used to add the column names to subject_train, X_train, y_train, subject_test, X_test,
+	y_test and to change column 3 and 4 names in the narrow_tidy_data dataframe 
+cbind() used to combine the subject_train, X_train y_train to form train and the 
+	subject_test X_test y_test to form test.
+rbind() used to combine the test and train dataframes
+grepl() used to extract the mean and std data from all_data
+cbind() used to combine the subject, activity, mean and std dataframes
+levels() used to name the activities
+melt() used to create a long thin table to use dcast() on to get the means and to produce narrow_tidy_data
+dcast() used with mean() to get the activity means
+write.table() used to output the narrow_tidy_data.txt file into the working directory.
+
+
+
+
+
---
+++ run_analysis.R
@@ -0,0 +1,124 @@
+########################################################
+####     Getting & Cleaning Data Project
+########################################################
+getwd()
+setwd("GCD_Assignment")
+getwd()
+
+########################################################
+###   read in the data - assumes files are in working directory
+########################################################
+
+### get training data
+train_subject<-read.table("./Data/train/subject_train.txt", quote = "\"'", 
+               na.strings = "NA",blank.lines.skip = TRUE)
+X_train<-read.table("./Data/train/X_train.txt")
+y_train<-read.table("./Data/train/y_train.txt")
+
+### get test data
+test_subject<-read.table("./Data/test/subject_test.txt")
+X_test<-read.table("./Data/test/X_test.txt")
+y_test<-read.table("./Data/test/y_test.txt")
+
+## get Features.txt for X_train/test variable names
+features<-read.table("features.txt",header = FALSE, sep = "", quote = "\"'" )
+
+## get just names and convert to character 
+VarNames<-as.character(features[,2])
+
+## get activities
+activities<-read.table("activity_labels.txt")
+
+
+########################################################
+### Merge data to form training and test data then all data
+### Giving columns names is done here
+########################################################
+
+## add headers from features.txt the column names in x_train & test
+names(X_train)<-VarNames
+names(X_test)<-VarNames
+
+## header for y_train & test
+names(y_test)<- "Activity"
+names(y_train)<- "Activity"
+
+## header for subject 
+names(train_subject) <- "Subject"
+names(test_subject) <- "Subject"
+
+## combine all test data test contains 2947 obs. of 563 var
+test<-cbind(test_subject,y_test,X_test, deparse.level=1)
+
+## combine all the training data
+train<-cbind(train_subject,y_train,X_train, deparse.level=1)
+
+## combine training and test data
+all_data<-rbind(train,test)
+
+## should now have a data frame with 563 variables and 10 299 observations
+
+########################################################
+## now get just columns with names containing mean or std
+########################################################
+
+## 46 variables
+m_data<-all_data[grepl("mean", names(all_data), ignore.case = FALSE, perl = FALSE,
+                                 fixed = FALSE, useBytes = FALSE)]
+## removes meanFreq variables - 33 variables
+mean_data<-m_data[!grepl("meanFreq", names(m_data), ignore.case = FALSE, perl = FALSE,
+                               fixed = FALSE, useBytes = FALSE)]
+
+## 33 variables
+std_data<-all_data[grepl("std", names(all_data), ignore.case = FALSE, perl = FALSE,
+                               fixed = FALSE, useBytes = FALSE)]
+
+## combine std and mean data n.b. subject and activity removed so also have to add those too
+
+tidy_data<-cbind(all_data[,1:2], mean_data, std_data)  ## 10299 obs of 68 variables
+
+########################################################
+### Give the activity column names rather than numbers
+########################################################
+
+## make activities column into factors
+tidy_data[,2]<- as.factor(tidy_data[,2])
+
+## change the factor numbers to activities - only 6 so do individually
+levels(tidy_data[,2])[1] <- "WALKING"
+levels(tidy_data[,2])[2] <- "WALKING_UPSTAIRS"
+levels(tidy_data[,2])[3] <- "WALKING_DOWNSTAIRS"
+levels(tidy_data[,2])[4] <- "SITTING"
+levels(tidy_data[,2])[5] <- "STANDING"
+levels(tidy_data[,2])[6] <- "LYING"
+
+## order the data using subject and activity
+library(plyr)
+tidy_data<-arrange(tidy_data, Subject, Activity)
+
+########################################################
+## Get the average for each activity per subject - 30 x 5 rows
+########################################################
+library(reshape2)
+
+## melt data to get long thin table with 4 var (subject activity, measurement and value)
+mtd<-melt(tidy_data, id=c("Subject", "Activity"))
+
+## get the mean of the measurement data - 
+## 180 obs of 68 variables - one row for each subject and activity, with many measurements
+## leave variable names as they are
+wide_tidy_data<-dcast(mtd, Subject + Activity ~ variable, mean)
+
+
+# 11880 obs of 4 var one row for each subject, activity and measurement type
+narrow_tidy_data <- melt(wide_tidy_data, id=c("Subject", "Activity"))
+
+# change names of variables to make them more meaningful
+names(narrow_tidy_data)[3]<- "measurement_type"
+names(narrow_tidy_data)[4]<- "measurement"
+
+## write the narrow data set to file for project, row_names=FALSE stops the rows being numbered 
+write.table(narrow_tidy_data,file="narrow_tidy_data.txt", row.names= FALSE, append=FALSE)
+
+t<-read.table("narrow_tidy_data.txt", quote = "\"'", 
+                             na.strings = "NA",blank.lines.skip = TRUE)
